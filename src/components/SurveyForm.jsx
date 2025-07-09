1c7d6f2e5b6aa122db777426a3c33f3911e0b55a
Leo-alt-f4 <leocunha.cabral@gmail.com>
2025-07-08 23:51:09 -0300

    Updated SurveyForm.jsx

--- src/components/SurveyForm.jsx
+++ src/components/SurveyForm.jsx
@@ -8,17 +8,5 @@
 -        <p>
 -            O GUIDO (Grupo de Pais e Amigos pela Unidade InfantoJuvenil de Onco-hematologia) é um grupo de pessoas dedicadas, profissionais de saúde, pais e voluntários que se reuniram para apoiar as famílias que viajavam de longe em busca de tratamento para seus filhos.
 -        </p>
--          <br />
--          <p className="text-pink-50 leading-relaxed">
--            O GUIDO evoluiu para a Casa GUIDO, uma OSC comprometida com o bem-estar de crianças e adolescentes com câncer. Desde 2010, temos sido um farol de esperança para famílias em busca de tratamento. Nossa casa acolhedora oferece não apenas tratamento médico, mas também amor, apoio e refeições para aqueles que estão longe de casa. Ajudamos com o transporte, medicamentos, terapias e educação, criando um ambiente onde o sorriso é mais forte do que o câncer.
--          </p>
--          <br />
--          <p className="text-pink-50 leading-relaxed">
--            A Casa GUIDO é uma luz na escuridão do câncer pediátrico. Somos um lembrete de que, juntos, podemos superar qualquer desafio e encher os corações de crianças com a luz da esperança. Junte-se a nós nesta jornada de amor e cura.
--          </p>
--        </div>
--        
--        <p className="text-pink-100 text-lg leading-relaxed">
--          Sua avaliação nos ajuda a continuar oferecendo o melhor cuidado e apoio para crianças e adolescentes (0-19 anos) em tratamento oncológico e suas famílias.
--        </p>
+-          
        </div>
